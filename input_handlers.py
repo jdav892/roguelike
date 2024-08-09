06f1e5ccc72dce5bd004c06c92ebd9c77e167868
jdav892 <jadavila9@gmail.com>
2024-08-09 03:06:01 -0400

    methods added

--- input_handlers.py
+++ input_handlers.py
@@ -4,3 +4,28 @@ import tcod.event
 
 from actions import Action, EscapeAction, MovementAction
 
+class EventHandler(tcod.event.EventDispatch[Action]):
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        #Quit event when we click "X" window of the program
+        raise SystemExit()
+    
+    def ev_keydown(self, event: tcod.eventKeyDown) -> Optional[Action]:
+        #Set to None if no key valid key is pressed.
+        action: Optional[Action] = None
+        
+        key = event.sym
+        #Movement keys based on Up, Down, Left, Right movements.
+        if key == tcod.event.K_UP:
+            action = MovementAction(dx = 0, dy = -1)
+        elif key == tcod.event.K_DOWN:
+            action = MovementAction(dx = 0, dy = 1)
+        elif key == tcod.event.K_LEFT:
+            action = MovementAction(dx = -1, dy = 0)
+        elif key == tcod.event.K_RIGHT:
+            action = MovementAction(dx = 1, dy = 0)
+            
+        elif key == tcod.event.K_ESCAPE:
+            action = EscapeAction
+            
+        return action
+        
