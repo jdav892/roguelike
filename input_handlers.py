355435d7c695048bbffc82105826ea66d5714bf2
jdav892 <jadavila9@gmail.com>
2024-08-09 03:07:15 -0400

    comments

--- input_handlers.py
+++ input_handlers.py
@@ -23,8 +23,9 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             action = MovementAction(dx = -1, dy = 0)
         elif key == tcod.event.K_RIGHT:
             action = MovementAction(dx = 1, dy = 0)
-            
+
         elif key == tcod.event.K_ESCAPE:
+            #Escape key press exits the game through returning the EscapeAction
             action = EscapeAction
             
         return action
