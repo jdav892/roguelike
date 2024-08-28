7d1e7ef8b094bc75c429c06a14006ed6544bef73
jdav892 <jadavila9@gmail.com>
2024-08-28 14:02:13 -0400

    changes to procgen

--- procgen.py
+++ procgen.py
@@ -8,7 +8,7 @@ import tile_types
 
 
 if TYPE_CHECKING:
-    from entity import Entity
+    from engine import Engine
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -81,10 +81,11 @@ def generate_dungeon(
     map_width: int,
     map_height: int,
     max_monsters_per_room: int,
-    player: Entity,
+    engine: Engine,
 ) -> GameMap:
     """Generate a new map."""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
     
     rooms: List[RectangularRoom] = []
     
@@ -107,6 +108,7 @@ def generate_dungeon(
         
         if len(rooms) == 0:
             #The first room, where player starts.
+            player.place(*new_room.center, dungeon)
             player.x, player.y = new_room.center
         else: #All rooms after the first.
             #Dig a tunnel between this and previous rooms.
