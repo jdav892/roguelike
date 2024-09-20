61ca5739cc5349bbfda118f9a7290e8f4e90f019
jdav892 <jadavila9@gmail.com>
2024-09-20 05:06:06 -0400

    new movement keys made

--- input_handlers.py
+++ input_handlers.py
@@ -3,10 +3,38 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
-from actions import Action, BumpAction, EscapeAction
+from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
+    
+MOVEMENT_KEYS = {
+    #Arrow keys.
+    tcod.event.KeySym.UP: (0, -1),
+    tcod.event.KeySym.DOWN: (0, 1),
+    tcod.event.KeySym.LEFT: (-1, 0),
+    tcod.event.KeySym.RIGHT: (1, 0),
+    tcod.event.KeySym.HOME: (-1, -1),
+    tcod.event.KeySym.END: (1, -1),
+    tcod.event.KeySym.PAGEUP: (1, -1),
+    tcod.event.KeySym.PAGEDOWN: (1, 1),
+    #Numpad keys.
+    tcod.event.KeySym.KP_1: (-1, 1),
+    tcod.event.KeySym.KP_2: (0, 1),
+    tcod.event.KeySym.KP_3: (1, 1),
+    tcod.event.KeySym.KP_4: (-1, 0),
+    tcod.event.KeySym.KP_6: (1, 0),
+    tcod.event.KeySym.KP_7: (-1, -1),
+    tcod.event.KeySym.KP_8: (0, -1),
+    tcod.event.KeySym.KP_9: (1, -1)
+}
+
+WAIT_KEYS = {
+    tcod.event.KeySym.KP_PERIOD,
+    tcod.event.KeySym.KP_5,
+    tcod.event.KeySym.K_CLEAR,
+}
+
 
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
@@ -38,15 +66,15 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         player = self.engine.player
         
         #Movement keys based on Up, Down, Left, Right movements.
-        if key == tcod.event.KeySym.UP:
-            action = BumpAction(player, dx = 0, dy = -1)
-        elif key == tcod.event.KeySym.DOWN:
-            action = BumpAction(player, dx = 0, dy = 1)
-        elif key == tcod.event.KeySym.LEFT:
-            action = BumpAction(player, dx = -1, dy = 0)
-        elif key == tcod.event.KeySym.RIGHT:
-            action = BumpAction(player, dx = 1, dy = 0)
-
+        #if key == tcod.event.KeySym.UP:
+        #    action = BumpAction(player, dx = 0, dy = -1)
+        #elif key == tcod.event.KeySym.DOWN:
+        #    action = BumpAction(player, dx = 0, dy = 1)
+        #elif key == tcod.event.KeySym.LEFT:
+        #    action = BumpAction(player, dx = -1, dy = 0)
+        #elif key == tcod.event.KeySym.RIGHT:
+        #    action = BumpAction(player, dx = 1, dy = 0)
+        
         elif key == tcod.event.KeySym.ESCAPE:
             #Escape key press exits the game through returning the EscapeAction
             action = EscapeAction(player)
