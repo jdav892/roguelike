c097ee8dcd0bb8d0d2b747b3d63165a5ffcef4cb
jdav892 <jadavila9@gmail.com>
2024-09-22 07:25:26 -0400

    entity modified to handle inventory behaviors:

--- entity.py
+++ entity.py
@@ -8,6 +8,7 @@ if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
     from components.fighter import Fighter
+    from components.inventory import Inventory
     from game_map import GameMap
     
 T = TypeVar("T", bound="Entity")
@@ -79,7 +80,8 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
-        fighter: Fighter
+        fighter: Fighter,
+        inventory: Inventory,
     ):
         super().__init__(
             x = x,
@@ -95,6 +97,9 @@ class Actor(Entity):
         
         self.fighter = fighter
         self.fighter.parent = self
+        
+        self.inventory = inventory
+        self.inventory.parent = self
         
     @property
     def is_alive(self) -> bool:
