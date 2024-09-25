0bb43692602721a574786180fc5280c388476c05
jdav892 <jadavila9@gmail.com>
2024-09-25 07:50:24 -0400

    updated methods imported from tcod

--- setup_game.py
+++ setup_game.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import copy
 from typing import Optional
 import tcod
+from tcod import libtcodpy
 import color
 from engine import Engine
 import entity_factories
@@ -54,16 +55,16 @@ class MainMenu(input_handlers.BaseEventHandler):
         console.print(
             console.width // 2,
             console.height // 2 - 4,
-            "Tombs of the Forgotten Kings",
+            "Crypt of The Void Millennia",
             fg=color.menu_title,
-            alignment=tcod.CENTER,
+            alignment=libtcodpy.CENTER,
         )
         console.print(
             console.width // 2,
             console.height - 2,
             "By Justin Davila",
             fg=color.menu_title,
-            alignment=tcod.CENTER,
+            alignment=libtcodpy.CENTER,
         )
         
         menu_width = 24
@@ -76,8 +77,8 @@ class MainMenu(input_handlers.BaseEventHandler):
                 text.ljust(menu_width),
                 fg=color.menu_text,
                 bg=color.black,
-                alignment=tcod.CENTER,
-                bg_blend=tcod.BKGND_ALPHA(64)
+                alignment=libtcodpy.CENTER,
+                bg_blend=libtcodpy.BKGND_ALPHA(64)
             )
     def ev_keydown(
         self, event: tcod.event.KeyDown
