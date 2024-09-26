3b90db14c47d9691b165d48a0a6d18407e6946b9
jdav892 <jadavila9@gmail.com>
2024-09-26 18:05:11 -0400

    menu created with input options where player must select and option to exit

--- input_handlers.py
+++ input_handlers.py
@@ -169,6 +169,69 @@ class AskUserEventHandler(EventHandler):
            By default this returns to the main event handler.
         """
         return MainGameEventHandler(self.engine)
+    
+class LevelUpEventHandler(AskUserEventHandler):
+    TITLE = "Level Up"
+    
+    def on_render(self, console: tcod.Console) -> None:
+        super().on_render(console)
+        
+        if self.engine.player.x <= 30:
+            x = 40
+        else:
+            x = 0
+            
+        console.draw_frame(
+            x=x,
+            y=0,
+            width=35,
+            height=8,
+            title=self.TITLE,
+            clear=True,
+            fg=(255, 255, 255),
+            bg=(0, 0, 0),
+        )
+        
+        console.print(
+            x=x + 1,
+            y=4,
+            string=f"a) Constitution(+20 HP, from {self.engine.player.fighter.max_hp})",
+        )
+        console.print(
+            x=x + 1,
+            y=5,
+            string=f"b) Strength(+1 attack, from {self.engine.player.fighter.power})",
+        )
+        console.print(
+            x=x + 1,
+            y=6,
+            string=f"c) Agility(+1 defense, from {self.engine.player.fighter.defense})",
+        )
+        
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[ActionOrHandler]:
+        player = self.engine.player
+        key = event.sym
+        index = key - tcod.event.KeySym.a
+        
+        if 0 <= index <= 2:
+            if index == 0:
+                player.level.increase_max_hp()
+            elif index == 1:
+                player.level.increase_power()
+            else:
+                player.level.increase_defense()
+        else:
+            self.engine.message_log.add_message("Invalid entry", color.invalid)
+            
+            return None
+
+        return super().ev_keydown(event)
+    
+    def ev_mousebuttondown(
+        self, event: tcod.event.MouseButtonDown
+    ) -> Optional[ActionOrHandler]:
+        #Don't allow the player to click to exit the menu like normal
+        return None
 
 class InventoryEventHandler(AskUserEventHandler):
     """This handler will let the user select an item
